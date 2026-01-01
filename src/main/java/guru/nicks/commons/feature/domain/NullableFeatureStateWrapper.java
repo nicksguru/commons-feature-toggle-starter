c2449d4a5c9c7f5f35df416113000809b5590d3c
Nick S. <job-offers@nicks.guru>
2026-01-01 20:44:14 +0300

    fix(core): added Serializable to record classes

--- src/main/java/guru/nicks/commons/feature/domain/NullableFeatureStateWrapper.java
+++ src/main/java/guru/nicks/commons/feature/domain/NullableFeatureStateWrapper.java
@@ -4,9 +4,11 @@ import jakarta.annotation.Nullable;
 import lombok.Builder;
 import org.togglz.core.repository.FeatureState;
 
+import java.io.Serializable;
+
 @Builder(toBuilder = true)
 public record NullableFeatureStateWrapper(
 
         @Nullable
-        FeatureState featureState) {
+        FeatureState featureState) implements Serializable {
 }
